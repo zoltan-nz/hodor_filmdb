bf441a1095a55c82eae345ca2a38c793a4d29e75
Zoltan <zdebre@gmail.com>
2013-06-13 16:46:44 +0100

    film.rb update with writer

--- app/models/film.rb
+++ app/models/film.rb
@@ -2,4 +2,15 @@ class Film < ActiveRecord::Base
   has_many :roleships
   has_many :participants, through: :roleships
   has_many :roles, through: :roleships
+
+  def writers
+
+  end
+
+  def writers=(participant)
+    @writer_role = Role.find(1)
+    self.roles << @writer_role
+    self.participants << participant
+  end
+
 end
